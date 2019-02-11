2ae27223763f251e43c7a3a1a821532bd774d796
Alex Forrence <aforren1@jhu.edu>
2019-02-11 12:22:51 -0500

    Cleanup, add requirements.txt

--- mab.py
+++ mab.py
@@ -1,145 +1,214 @@
+import os
 import sys
+from string import ascii_lowercase
+from datetime import datetime
 
 from collections import OrderedDict
 import numpy as np
 import pandas as pd
-import yaml
 from psychopy.visual.rect import Rect
 from psychopy import core, visual, event, gui
-from slotmachine import SlotMachine
+from make_seq import make_seq
+from feedback import Feedback
 
-# settings
-settings = OrderedDict({'subject': '001', 'file': 'foo.csv'})
-
-try:
-    with open('user_settings.yml', 'r') as f:
-        potential_settings = yaml.load(f)
-    if potential_settings.keys() == settings.keys():
-        for i in settings.keys():
-            settings[i] = potential_settings[i]
-except (FileNotFoundError, AttributeError):
-    pass
-
+settings = OrderedDict({'subject': '001'})
+dt = datetime.now().strftime('%Y%m%d-%H%M%S')
 dlg = gui.DlgFromDict(settings, title='Experiment')
+
 if not dlg.OK:
     sys.exit()
 
-settings = dict(settings)
-with open('user_settings.yml', 'w') as f:
-    yaml.dump(settings, f, default_flow_style=False)
+# generate tables
+blocks_per_section = 1  # 6 for real
+practice = 3  # 50 for real
+trials = 3  # 200 for real
+seeds = list(range(blocks_per_section))
+possible_amounts = []
 
-trials = pd.read_csv(settings['file'], header=None)
-trials = trials.to_dict('records')
-# all done settings, now onto the experiment
-win = visual.Window(units='height', fullscr=True, allowGUI=False, multiSample=True, numSamples=8)
+blocks = []
+# 4-choice
+# warmup
+blocks.append((4, make_seq(filename=None, num_resp=4, num_trials=practice, seed=100)))
 
-choices = ['h', 'u', 'i', 'l']
-#grid = [(-0.2, 0.2), (0.2, 0.2), (-0.2, -0.2), (0.2, -0.2)]
-grid = [(-0.5, -0.1), (-0.166, -0.1), (0.166, -0.1), (0.5, -0.1)]
-colors = ['yellow', 'blue', 'red', 'green']
-slots = []
-for i, j, k in zip(choices, grid, colors):
-    slots.append(SlotMachine(win, id=i.upper(), colour=k, pos=j, size=0.2))
+for i in seeds:
+    blocks.append((4, make_seq(filename=None, num_resp=4, num_trials=trials, seed=i)))
 
-x_img = visual.ImageStim(win, image='x_small.png', pos=(0, 0), size=0.3, opacity=0.8)
+# 8-choice
+for i in seeds:
+    blocks.append((8, make_seq(filename=None, num_resp=8, num_trials=trials, seed=i+10)))
 
-# intro screen
-info_text = visual.TextStim(win, text='Press any key to start.',
-                            pos=(0, 0), alignHoriz='center', alignVert='center', height=0.1, color='white')
+# 26-choice
+for i in seeds:
+    blocks.append((26, make_seq(filename=None, num_resp=26, num_trials=trials, seed=i+10)))
 
-info_text.draw()
+blocks = [(i, x.to_dict('records')) for i, x in blocks]
+keys = {}
+keys[4] = ['h', 'u', 'i', 'l']
+keys[8] = ['a', 'w', 'e', 'f'] + keys[4]
+keys[26] = list(ascii_lowercase)
+
+# set up visuals
+win = visual.Window(units='height', fullscr=True, allowGUI=False)
+
+feedback = Feedback(win)
+
+center_sets = {'pos': (0, 0), 'alignHoriz': 'center', 'alignVert': 'center',
+               'height': 0.05, 'color': 'white'}
+remind_sets = center_sets.copy()
+remind_sets['pos'] = (0, -0.2)
+intro_txt = visual.TextStim(win, text='Press any key to start.', **center_sets)
+
+break_txt = visual.TextStim(win, text='Take a break. Press any key to continue.',
+                            **center_sets)
+
+instr_txt = {}
+instr_txt[4] = visual.TextStim(win, text=('For this 4-choice section,'
+                                          '\nuse the keys H, U, I, and L.'
+                                          '\nGet the highest score you can.'
+                                          '\nPress any key to start.'),
+                               **center_sets)
+
+instr_txt[8] = visual.TextStim(win, text=('For this 8-choice section,'
+                                          '\nuse the keys A, W, E, F, H, U, I, and L.'
+                                          '\nGet the highest score you can.'
+                                          '\nPress any key to start.'),
+                               **center_sets)
+
+instr_txt[26] = visual.TextStim(win, text=('For this 26-choice section,'
+                                           '\nuse all letters of the alphabet.'
+                                           '\nGet the highest score you can.'
+                                           '\nPress any key to start.'),
+                                **center_sets)
+
+remind_txt = {}
+remind_txt[4] = visual.TextStim(win, text='H, U, I, L', **remind_sets)
+
+remind_txt[8] = visual.TextStim(win, text='A, W, E, F, H, U, I, L',
+                                **remind_sets)
+
+remind_txt[26] = visual.TextStim(win, text='Entire Alphabet', **remind_sets)
+
+wait = visual.Rect(win, width=0.4, height=0.1, pos=(0, -0.3), lineColor=None)
+choice_txt = visual.TextStim(win, text='F', pos=(0, 0), alignHoriz='center',
+                             alignVert='center', height=0.05, color='white')
+intro_txt.draw()
 win.flip()
-
-while not event.getKeys():
-    pass
-
-for i in slots:
-    i.draw()
-
-win.flip()
+event.waitKeys()
 
 trial_timer = core.Clock()
 
-
-def reset_timer(trial_timer, val):
-    trial_timer.reset(val)
-
-
 total_points = 0
-max_score = 0
+max_possible = 0
+block_count = 0
 
-data = {'subject': settings['subject'], 'choice': None}  # tack on columns
-data_filename = 'subject%s_%s' % (settings['subject'], settings['file'])
-data_list = []
-
-for trial in trials:  # for i in trials
-    # start with blank screen for 1s
-    # draw all stim
-    if event.getKeys(['esc', 'escape']):
-        core.quit()
-    for i in slots:
-        i.draw()
-    win.callOnFlip(reset_timer, trial_timer, 0)
-    win.callOnFlip(event.clearEvents)
+for number_choices, block_table in blocks:
+    # intro text
+    instr_txt[number_choices].draw()
     win.flip()
-    while trial_timer.getTime() <= 1.5:
-        resp = event.getKeys(choices, timeStamped=trial_timer)
-        if resp:
-            break
 
-    if not resp:  # 2s penalty
-        # show big red X
-        x_img.draw()
+    event.waitKeys()
+    block_data = []
+    # loop through trials within a block
+    for trial in block_table:
+        if event.getKeys(['esc', 'escape']):
+            core.quit()
+        feedback.state = 'idle'
+        feedback.draw()
+        remind_txt[number_choices].draw()
+        win.callOnFlip(trial_timer.reset, 0)
         win.flip()
-        core.wait(2)
-        max_score += 100
-        tmp_data = {'subject': settings['subject'],
-                    'choice': -1,
-                    'reaction_time': -1,
-                    'points': 0}
-        tmp_data.update(trial)
-        data_list.append(tmp_data)
-    else:
-        choice, val = resp[0]
-        idx = choices.index(choice)
-        slots[idx].toggle_id(False)
-        slots[idx].toggle_ani(True)
-        slots[idx].toggle_arm(30)
+
+        choice, val = event.waitKeys(keyList=keys[number_choices],
+                                     timeStamped=trial_timer,
+                                     clearEvents=True)[0]
+        idx = keys[number_choices].index(choice)
         trial_timer.reset(0)
-        while trial_timer.getTime() <= 1:
-            for i in slots:
-                i.draw()
+        feedback.state = 'ani'
+        choice_txt.setText(choice.upper())
+        while trial_timer.getTime() <= 0.5:
+            choice_txt.pos = (0.2, 0)
+            choice_txt.draw()
+            choice_txt.pos = (-0.2, 0)
+            choice_txt.draw()
+            feedback.draw()
+            remind_txt[number_choices].draw()
             win.flip()
-        slots[idx].toggle_ani(False)
-        slots[idx].toggle_points(True)
-        slots[idx].update_points_text(trial[idx])
+        feedback.state = 'points'
+        feedback.points = trial[idx]
         total_points += trial[idx]
-        max_score += 100
-        tmp_data = {'subject': settings['subject'],
-                    'choice': idx,
-                    'reaction_time': val,
-                    'points': trial[idx]}
-        tmp_data.update(trial)
-        data_list.append(tmp_data)
-        for i in slots:
-            i.draw()
-        win.flip()
-        core.wait(1 - 1/60)
-        slots[idx].toggle_arm(-30)
-        slots[idx].toggle_points(False)
-        slots[idx].toggle_id(True)
-        for i in slots:
-            i.draw()
-        win.flip()
-        #core.wait(0.5 - 1/60)
+        max_possible += max(trial.values())
+        trial_data = {'subject': settings['subject'],
+                      'choice': idx,
+                      'reaction_time': val,
+                      'points': trial[idx],
+                      'total_points': total_points,
+                      'max_possible': max_possible}
+        trial_data.update(trial)
+        block_data.append(trial_data)
 
-info_text.text = 'Score: %i' % total_points
-info_text.draw()
+        feedback.draw()
+        remind_txt[number_choices].draw()
+        win.flip()
+        core.wait(0.5 - 1/60)
+        feedback.state = 'cooldown'
+        feedback.draw()
+        remind_txt[number_choices].draw()
+        win.flip()
+        # core.wait(0.2)
+
+    prop_of_possible = round(float(total_points)/float(max_possible), 2)
+    possible_amounts.append(prop_of_possible)
+    intro_txt.text = ('Proportion of max score: %s\n'
+                      'Take a break now.') % prop_of_possible
+    intro_txt.draw()
+    wait.fillColor = 'red'
+    wait.draw()
+    win.flip()
+    core.wait(5)
+    intro_txt.text = ('Proportion of max score: %s\n'
+                      'Take a break now, and\n'
+                      'press any key to continue.') % prop_of_possible
+    intro_txt.draw()
+    wait.fillColor = 'green'
+    wait.draw()
+    win.flip()
+
+    directory = 'data/%s' % settings['subject']
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    dat = pd.DataFrame(block_data)
+    filename = '%s_subject%s_block%i_%ichoice.csv' % (dt, settings['subject'],
+                                                      block_count, number_choices)
+    dat.to_csv(os.path.join(directory, filename), sep=',', index=False)
+    block_count += 1
+    total_points = 0
+    max_possible = 0
+    event.waitKeys()
+
+# monies
+# $12-18
+brackets = [([0, 1./7], 12),
+            ([1./7, 2./7], 13),
+            ([2./7, 3./7], 14),
+            ([3./7, 4./7], 15),
+            ([4./7, 5./7], 16),
+            ([5./7, 6./7], 17),
+            ([6./7, 1], 18)]
+
+cash_to_pay = 15
+cash_to_pay = np.random.choice(possible_amounts)
+print(possible_amounts)
+print(cash_to_pay)
+for fracs, val in brackets:
+    if prop_of_possible >= fracs[0] and prop_of_possible < fracs[1]:
+        cash_to_pay = val
+
+print('You made %i dollars.' % cash_to_pay)
+intro_txt.text = 'You made %i dollars. Press esc to exit.' % cash_to_pay
+intro_txt.draw()
 win.flip()
+event.waitKeys(keyList=['esc', 'escape'])
 
-core.wait(4)
-
-dat = pd.DataFrame(data_list)
-dat.to_csv(data_filename, sep=',', index=False)
 
 core.quit()
